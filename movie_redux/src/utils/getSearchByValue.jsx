4fa47c27c0b81f366730ccf174bb18af811c67c6
surekha <surekharangam51@gmail.com>
2025-07-28 12:05:44 +0530

    add changes in utils

--- movie_redux/src/utils/getSearchByValue.jsx
+++ movie_redux/src/utils/getSearchByValue.jsx
@@ -1,6 +1,6 @@
-export const getSearchByValue=(movies,value)=>{
-    
+export const getSearchByValue = (movies, value) => {
 
-    const filterData=value?.length>0 ? (  movies.filter((movie)=>movie.movieName.toLowerCase().includes(value.toLowerCase()) || movie.directorName.toLowerCase().includes(value.toLowerCase()))) : movies 
+
+    const filterData = value?.length > 0 ? (movies.filter((movie) => movie.movieName.toLowerCase().includes(value.toLowerCase()) || movie.directorName.toLowerCase().includes(value.toLowerCase()))) : movies
     return filterData;
 }
